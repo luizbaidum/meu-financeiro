13d5448a860545a90a17114027dc567e42c8b060
luizbaidum <luiz.baidum@hotmail.com.br>
2024-01-27 14:42:50 -0300

    uploading main folders and files

---
+++ public/header.php
@@ -0,0 +1,9 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+
+    <title>Title</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css.map">
+</head>
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php require_once ("header.php"); ?>
+<body>
+
+    <main id="grid-principal" class="container">
+        teste
+    </main>
+
+</body>
+</html>
