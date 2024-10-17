169d81aec067e14d2dbff0e8433ef9e8aae77b0a
luizbaidum <luiz.baidum@hotmail.com.br>
2024-10-17 16:10:58 -0300

    definição tela cadastro orçamento

--- htdocs/public/orcamento.php
+++ htdocs/public/orcamento.php
@@ -11,39 +11,27 @@
         <div class="card">
             <div class="card-header">Importar</div>
             <div class="card-body">
-                <form action="../sql/cad_orcamento.php" method="post">
+                <form data-url-action="../sql/cad_orcamento.php" data-method="POST" class="form-ajax">
                     <div class="form-group">
+                        <input type="hidden" name="buscarImportacao" value="1">
                         <div class="row">
                             <div class="col-6">
-                                <label for="idCategoria">Mês origem</label>
-                                <select class="form-select" id="idCategoria" name="idCategoria" required>
-                                    <option value="">Selecione</option>
-                                    <?php 
-                                        $categorias = $crud->selectAll("categoria", [], [], ["categoria" => "ASC"]);
-                                        foreach ($categorias as $cat):
-                                    ?>
-                                        <option value="<?= $cat["idCategoria"]; ?>"><?= $cat["categoria"] . " - " . $cat["tipo"]; ?></option>
-                                    <?php endforeach; ?>
-                                </select>
+                                <label for="idOrigem">Mês origem</label>
+                                <input type="month" id="idOrigem" name="origem" placeholder="yyyy-mm">
                             </div>
                             <div class="col-6">
-                                <label for="idCategoria">Mês destino</label>
-                                <select class="form-select" id="idCategoria" name="idCategoria" required>
-                                    <option value="">Selecione</option>
-                                    <?php 
-                                        $categorias = $crud->selectAll("categoria", [], [], ["categoria" => "ASC"]);
-                                        foreach ($categorias as $cat):
-                                    ?>
-                                        <option value="<?= $cat["idCategoria"]; ?>"><?= $cat["categoria"] . " - " . $cat["tipo"]; ?></option>
-                                    <?php endforeach; ?>
-                                </select>
+                                <label for="idDestino">Mês destino</label>
+                                <input type="month" id="idDestino" name="destino" placeholder="yyyy-mm">
                             </div>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-primary mt-1">Submit</button>
+                    <button type="submit" class="btn btn-primary mt-1">Buscar</button>
                 </form>  
             </div>
         </div>
+
+        <div class="content-importar"></div>
+
         <div class="card mt-2">
             <div class="card-header">Cadastro</div>
             <div class="card-body">
