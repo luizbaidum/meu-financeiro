f92a9b69441be58159ab187cd11e258a36921eff
luizbaidum <luiz.baidum@hotmail.com.br>
2024-10-17 15:31:10 -0300

    separação tela orçamentos

--- htdocs/public/orcamento.php
+++ htdocs/public/orcamento.php
@@ -2,17 +2,49 @@
     require 'header.php';
 
     $crud = new CRUD();
-    $orcamentos = $crud->orcamentos($_POST['mesFiltro'] ?? '');
 
-    $total_orcado = 0;
+    $months = array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'); 
 ?>
 
 <main class="container">
-	<form action="orcamento.php" data-method="post" id="idFormMesFiltro">
-        <?php require_once 'select_month.php'; ?>
-	</form>
 	<div class="card p-1">
         <div class="card">
+            <div class="card-header">Importar</div>
+            <div class="card-body">
+                <form action="../sql/cad_orcamento.php" method="post">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-6">
+                                <label for="idCategoria">Mês origem</label>
+                                <select class="form-select" id="idCategoria" name="idCategoria" required>
+                                    <option value="">Selecione</option>
+                                    <?php 
+                                        $categorias = $crud->selectAll("categoria", [], [], ["categoria" => "ASC"]);
+                                        foreach ($categorias as $cat):
+                                    ?>
+                                        <option value="<?= $cat["idCategoria"]; ?>"><?= $cat["categoria"] . " - " . $cat["tipo"]; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+                            <div class="col-6">
+                                <label for="idCategoria">Mês destino</label>
+                                <select class="form-select" id="idCategoria" name="idCategoria" required>
+                                    <option value="">Selecione</option>
+                                    <?php 
+                                        $categorias = $crud->selectAll("categoria", [], [], ["categoria" => "ASC"]);
+                                        foreach ($categorias as $cat):
+                                    ?>
+                                        <option value="<?= $cat["idCategoria"]; ?>"><?= $cat["categoria"] . " - " . $cat["tipo"]; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-1">Submit</button>
+                </form>  
+            </div>
+        </div>
+        <div class="card mt-2">
             <div class="card-header">Cadastro</div>
             <div class="card-body">
                 <form action="../sql/cad_orcamento.php" method="post">
@@ -46,39 +78,6 @@
                     </div>
                     <button type="submit" class="btn btn-primary mt-1">Submit</button>
                 </form>  
-            </div>
-        </div>
-        <div class="card mt-2">
-            <div class="card-body">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Mês</th>
-                            <th scope="col">Categoria</th>
-                            <th scope="col">Valor Orçado</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($orcamentos as $value): 
-                            if ($value["tipo"] == "R")
-                                $total_orcado += $value["totalOrcado"];
-                            else
-                                $total_orcado -= $value["totalOrcado"];
-                        ?>
-                            <tr>
-                                <td><?= $value["mesOrcado"]; ?></td>
-                                <td><?= $value["categoria"]; ?></td>
-                                <td><?= ($value["tipo"] == "R" ? "+ " : "- ") . $value["totalOrcado"]; ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td colspan="2">Total</td>
-                            <td><?= $total_orcado; ?></td>
-                        </tr>
-                    </tfoot>
-                </table>
             </div>
         </div>
 	</div>
--- htdocs/sql/cad_orcamento.php
+++ htdocs/sql/cad_orcamento.php
@@ -3,8 +3,8 @@
 
     $crud = new CRUD();
 
-    if (!empty($_POST) && isset($_POST["dataOrcamento"]))
-        $crud->insert("orcamento", $_POST);
+    if (!empty($_POST) && isset($_POST['dataOrcamento']))
+        $crud->insert('orcamento', $_POST);
 
     header('location: ../public/orcamento.php');
 
