9be9ad2a9c0be5bd1d6f9810e9b37afebd785371
luizbaidum <luiz.baidum@hotmail.com.br>
2024-10-24 21:35:13 -0300

    cadastro orçamento em lote

--- htdocs/public/funcoes.php
+++ htdocs/public/funcoes.php
@@ -9,34 +9,68 @@ function buscarOrcamento($data)
 {
     $ano_busca = $data['anoOrigem'] ?? '';
     $mes_busca = $data['mesOrigem'] ?? '';
+    $destino = $data['destino'] ?? '';
 
     if ($ano_busca == '' && $mes_busca == '') {
+        return;
+    }
+
+    if ($destino == '') {
+        echo '<div class="card mt-2 text-danger text-center">Por favor, selecionar data destino do orçamento.</div>';
         return;
     }
 
     $crud = new CRUD();
     $resultado = $crud->buscarMediasDespesas($ano_busca, $mes_busca);
 
-    echo montarHtmlOrcamento($resultado);
+    echo montarHtmlOrcamento($resultado, $destino);
 }
 
-function montarHtmlOrcamento($dados)
+function montarHtmlOrcamento($dados, $data_destino)
 {
     if (empty($dados)) {
         return '<div class="card mt-2 text-center">Nenhum dado encontrado.</div>';
     }
-
-    $conteudo = '';
-
-    //montar cabeçalho e tabela
     
+    $cabecalho = '<thead>
+                    <tr>
+                        <th>selecionar</th>
+                        <th>Categoria</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>';
+
+    $conteudo = '<tbody>';
     foreach ($dados as $v) {
-        $conteudo .= '<td>a</td>';
+        $conteudo .= '<tr>
+                        <td>
+                            <input type="checkbox" value="' . $v['idCategoria'] . '" name="idCategoria[]">
+                        </td>
+                        <td>' . $v['idCategoria'] . ' - ' . $v['categoria'] . '</td>
+                        <td><input type="number" name="valor[]" value="' . $v['valorOrcamento'] . '"</td>
+                    </tr>';
     }
+    $conteudo .= '</tbody>';
 
-    $str = '<div class="card mt-2 text-center">
-                <table>' . $conteudo . '</table>
-        </div>';
+    $footer = '<tfoot>
+            </tfoot>';
 
-    return $str;
+    $tabela = "<table class='table'>
+                    $cabecalho $conteudo $footer
+                </table>";
+
+    $form = "<form action='../sql/cad_orcamento.php' method='post'>
+                $tabela
+                <input type='hidden' name='dataOrcamento' value='$data_destino'>
+                <input type='hidden' name='lote' value='T'>
+                <button type='submit' class='btn btn-primary mt-1'>Salvar</button>
+            </form>";
+
+    $card_table = "<div class='card mt-2'>
+                        <div class='card-body'>
+                            $form
+                        </div>
+                    </div>";
+
+    return $card_table;
 }
--- htdocs/sql/cad_orcamento.php
+++ htdocs/sql/cad_orcamento.php
@@ -3,8 +3,20 @@
 
     $crud = new CRUD();
 
-    if (!empty($_POST) && isset($_POST['dataOrcamento']))
-        $crud->insert('orcamento', $_POST);
+    if (!empty($_POST) && isset($_POST['dataOrcamento'])) {
+        if (isset($_POST['lote']) && $_POST['lote'] == 'T') {
+            foreach ($_POST['idCategoria'] as $k => $id_cat) {
+                $data['idCategoria'] = $id_cat;
+                $data['valor'] = $_POST['valor'][$k];
+                $data['dataOrcamento'] = $_POST['dataOrcamento'] . '-01';
+
+                $crud->insert('orcamento', $data);
+            }
+        } else {
+            $crud->insert('orcamento', $_POST);
+        }
+    }
+        
 
     header('location: ../public/orcamento.php');
 
